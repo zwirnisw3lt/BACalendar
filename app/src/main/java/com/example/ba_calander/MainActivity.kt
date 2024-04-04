5585f0cb71d53a21278746a6053dbb19da69d933
zwirnisw3lt <kevin@z-w3lt.de>
2024-04-04 11:19:03 +0200

    adds a viewmodel design and deletes the preview

--- app/src/main/java/com/example/ba_calander/MainActivity.kt
+++ app/src/main/java/com/example/ba_calander/MainActivity.kt
@@ -7,6 +7,7 @@ import android.provider.ContactsContract.CommonDataKinds.Website.URL
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -29,6 +30,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import com.example.ba_calander.ui.theme.BacalanderTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -45,6 +47,7 @@ import javax.net.ssl.SSLContext
 import javax.net.ssl.X509TrustManager
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -54,7 +57,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginView()
+                    LoginView(viewModel)
                 }
             }
         }
@@ -64,7 +67,7 @@ class MainActivity : ComponentActivity() {
 //hash: hash
 
 @Composable
-fun LoginView(modifier: Modifier = Modifier) {
+fun LoginView(viewModel : MainViewModel, modifier: Modifier = Modifier) {
     val (text1, setText1) = remember { mutableStateOf("number") }
     val (text2, setText2) = remember { mutableStateOf("hash") }
     val (checked, setChecked) = remember { mutableStateOf(false) }
@@ -103,98 +106,9 @@ fun LoginView(modifier: Modifier = Modifier) {
                 Text("Daten Speichern")
             }
 
-            Button(onClick = { showCalendar(context, prefs, checked, text1, text2) }) {
+            Button(onClick = { viewModel.showCalendar(context, prefs, checked, text1, text2) }) {
                 Text("Kalender Anzeigen")
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun LoginViewPreview() {
-    BacalanderTheme {
-        LoginView()
-    }
-}
-
-fun showCalendar(context: Context, preferences: SharedPreferences, checked: Boolean, text1: String, text2: String) {
-    if (checked) {
-        with (preferences.edit()) {
-            putString("text1", text1)
-            putString("text2", text2)
-            apply()
-        }
-    } else {
-
-    }
-
-    val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    val date = format.parse("2022-10-01 00:00:01") // Replace with your date string
-    val timestamp1 = date.time / 1000
-    val date2 = format.parse("2025-10-01 00:00:01") // Replace with your date string
-    val timestamp2 = date2.time / 1000
-
-    GlobalScope.launch(Dispatchers.IO) {
-        getPersonalCalendar(text1, text2, timestamp1, timestamp2)
-    }
-}
-
-fun getPersonalCalendar(user: String, hash: String, start: Long, end: Long) {
-    val maxAttempts = 3
-    var attempt = 0
-
-    while (attempt < maxAttempts) {
-        try {
-            val url = URL("https://selfservice.campus-dual.de/room/json?userid=${user}&hash=${hash}&start=${start}&end=${end}&_=1711960057052")
-            println(url)
-            // Create a TrustManager that trusts the server's certificate
-            val trustManager = object : X509TrustManager {
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-
-                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-
-                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-            }
-
-            // Create an SSLSocketFactory that uses our TrustManager
-            val sslContext = SSLContext.getInstance("TLS")
-            sslContext.init(null, arrayOf(trustManager), null)
-            val sslSocketFactory = sslContext.socketFactory
-
-            // Create an HttpsURLConnection using our SSLSocketFactory
-            val connection = url.openConnection() as HttpsURLConnection
-            connection.sslSocketFactory = sslSocketFactory
-
-            val responseCode = connection.responseCode
-            val responseMessage = connection.responseMessage
-
-            println("Response Code: $responseCode")
-            println("Response Message: $responseMessage")
-
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val response = connection.inputStream.bufferedReader().use { it.readText() }
-
-                println("Response: $response")
-
-                val jsonObject = JSONObject(response)
-
-                break
-
-            } else {
-                throw IOException("Unexpected response code: $responseCode")
-            }
-
-            connection.disconnect()
-        } catch (e: SocketTimeoutException) {
-            Log.e("identifier", "SocketTimeoutException: ${e.message}")
-            attempt++
-            if (attempt == maxAttempts) {
-                Log.e("identifier", "Failed to connect after $maxAttempts attempts")
-            }
-        } catch (e: IOException) {
-            Log.e("identifier", "IOException: ${e.message}")
-            break
         }
     }
 }
