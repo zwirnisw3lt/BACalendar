9da48a352f9f503c5a3dbe052d0aa84d6c46c964
zwirnisw3lt <kevin@z-w3lt.de>
2024-04-04 11:19:03 +0200

    adds a viewmodel design and deletes the preview

---
+++ app/src/main/java/com/example/ba_calander/MainViewModel.kt
@@ -0,0 +1,110 @@
+package com.example.ba_calander
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.security.cert.X509Certificate
+import java.text.SimpleDateFormat
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
+
+class MainViewModel : ViewModel() {
+    // Define your data and functions here
+    fun showCalendar(context: Context, preferences: SharedPreferences, checked: Boolean, text1: String, text2: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val text1 = preferences.getString("text1", "") ?: ""
+            val text2 = preferences.getString("text2", "") ?: ""
+
+            Log.d("identifier", "Text1: $text1")
+            Log.d("identifier", "Text2: $text2")
+
+            if (checked) {
+                with(preferences.edit()) {
+                    putString("text1", text1)
+                    putString("text2", text2)
+                    apply()
+                }
+            } else {
+            }
+            val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            val date = format.parse("2022-10-01 00:00:01") // Replace with your date string
+            val timestamp1 = date.time / 1000
+            val date2 = format.parse("2025-10-01 00:00:01") // Replace with your date string
+            val timestamp2 = date2.time / 1000
+
+            getPersonalCalendar(text1, text2, timestamp1, timestamp2)
+        }
+    }
+
+    fun getPersonalCalendar(user: String, hash: String, start: Long, end: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val maxAttempts = 3
+            var attempt = 0
+
+            while (attempt < maxAttempts) {
+                try {
+                    val url = URL("https://selfservice.campus-dual.de/room/json?userid=${user}&hash=${hash}&start=${start}&end=${end}&_=1711960057052")
+                    println(url)
+                    // Create a TrustManager that trusts the server's certificate
+                    val trustManager = object : X509TrustManager {
+                        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+
+                        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+
+                        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                    }
+
+                    // Create an SSLSocketFactory that uses our TrustManager
+                    val sslContext = SSLContext.getInstance("TLS")
+                    sslContext.init(null, arrayOf(trustManager), null)
+                    val sslSocketFactory = sslContext.socketFactory
+
+                    // Create an HttpsURLConnection using our SSLSocketFactory
+                    val connection = url.openConnection() as HttpsURLConnection
+                    connection.sslSocketFactory = sslSocketFactory
+
+                    val responseCode = connection.responseCode
+                    val responseMessage = connection.responseMessage
+
+                    println("Response Code: $responseCode")
+                    println("Response Message: $responseMessage")
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        val response = connection.inputStream.bufferedReader().use { it.readText() }
+
+                        println("Response: $response")
+
+                        val jsonObject = JSONObject(response)
+
+                        break
+
+                    } else {
+                        throw IOException("Unexpected response code: $responseCode")
+                    }
+
+                    connection.disconnect()
+                } catch (e: SocketTimeoutException) {
+                    Log.e("identifier", "SocketTimeoutException: ${e.message}")
+                    attempt++
+                    if (attempt == maxAttempts) {
+                        Log.e("identifier", "Failed to connect after $maxAttempts attempts")
+                    }
+                } catch (e: IOException) {
+                    Log.e("identifier", "IOException: ${e.message}")
+                    break
+                }
+            }
+        }
+    }
+}
