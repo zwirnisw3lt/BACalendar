8ffcb7635bf8459005985d2d21e050ec87597481
Kevin Böhme <kevin@z-w3lt.de>
2024-04-06 21:33:50 +0200

    adds a view which shows all the events

--- app/src/main/java/com/example/ba_calander/MainActivity.kt
+++ app/src/main/java/com/example/ba_calander/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.ba_calander
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Website.URL
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -16,99 +13,136 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import com.example.ba_calander.ui.theme.BacalanderTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import org.json.JSONObject
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.SocketTimeoutException
-import java.net.URL
-import java.security.cert.X509Certificate
-import java.text.SimpleDateFormat
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.X509TrustManager
+import kotlin.properties.Delegates
 
+enum class Screen {
+    LoginView,
+    CalendarView
+}
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BacalanderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LoginView(viewModel)
+            MyApp(viewModel)
+        }
+    }
+}
+
+@Composable
+fun MyApp(viewModel: MainViewModel) {
+    var currentScreen by remember { mutableStateOf(Screen.LoginView) }
+
+    BacalanderTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentScreen) {
+                Screen.LoginView -> LoginView(viewModel, { currentScreen = Screen.CalendarView })
+                Screen.CalendarView -> CalendarView(viewModel.events.value, { currentScreen = Screen.LoginView })
+            }
+        }
+    }
+}
+
+// TODO: HASH DELETE
+//hash: hash
+
+    @Composable
+    fun LoginView(
+        viewModel: MainViewModel,
+        onButtonClicked: () -> Unit,
+        modifier: Modifier = Modifier
+    ) {
+        val (text1, setText1) = remember { mutableStateOf("number") }
+        val (text2, setText2) = remember { mutableStateOf("hash") }
+        val (checked, setChecked) = remember { mutableStateOf(false) }
+        val context = LocalContext.current
+        val prefs = context.getSharedPreferences("MyApp", Context.MODE_PRIVATE)
+
+        Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Text(
+                    "Berufsakademie Kalender",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(16.dp)
+                )
+
+
+                OutlinedTextField(
+                    value = text1,
+                    onValueChange = setText1,
+                    label = { Text("Matrikelnummer") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    singleLine = true,
+                    maxLines = 1
+                )
+
+                OutlinedTextField(
+                    value = text2,
+                    onValueChange = setText2,
+                    label = { Text("Campus Dual Hash") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                    singleLine = true,
+                    maxLines = 1
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = setChecked,
+                    )
+                    Text("Daten Speichern")
+                }
+
+                viewModel.events.observeAsState().value?.let { events ->
+                    viewModel.events.value = events
+                }
+
+                Button(onClick = {
+                    viewModel.showCalendar(context, prefs, checked, text1, text2)
+                    onButtonClicked()
+                }) {
+                    Text("Kalender Anzeigen")
                 }
             }
         }
     }
-}
-// TODO: HASH DELETE
-//hash: hash
 
-@Composable
-fun LoginView(viewModel : MainViewModel, modifier: Modifier = Modifier) {
-    val (text1, setText1) = remember { mutableStateOf("number") }
-    val (text2, setText2) = remember { mutableStateOf("hash") }
-    val (checked, setChecked) = remember { mutableStateOf(false) }
-    val context = LocalContext.current
-    val prefs = context.getSharedPreferences("MyApp", Context.MODE_PRIVATE)
-
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-
-            Text("Berufsakademie Kalender", style = MaterialTheme.typography.headlineLarge, modifier = Modifier.padding(16.dp))
-
-            
-            OutlinedTextField(
-                value = text1,
-                onValueChange = setText1,
-                label = { Text("Matrikelnummer") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                singleLine = true,
-                maxLines = 1
-            )
-
-            OutlinedTextField(
-                value = text2,
-                onValueChange = setText2,
-                label = { Text("Campus Dual Hash") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                singleLine = true,
-                maxLines = 1
-            )
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = checked,
-                    onCheckedChange = setChecked,
-                )
-                Text("Daten Speichern")
-            }
-
-            Button(onClick = { viewModel.showCalendar(context, prefs, checked, text1, text2) }) {
-                Text("Kalender Anzeigen")
+    @Composable
+    fun CalendarView(
+        events: List<Event>,
+        onLogoutClicked: () -> Unit,
+        modifier: Modifier = Modifier) {
+        Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Calendar Ansicht")
+                events.forEach() {
+                    Text(it.title)
+                }
+                
+                Button(onClick = { onLogoutClicked() }) {
+                    Text("Logout")
+                }
             }
         }
     }
-}
