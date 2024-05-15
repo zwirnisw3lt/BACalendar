781d75fdc50f7d1f868e5ce928152111646f5826
zwirnisw3lt <kevin@z-w3lt.de>
2024-05-15 12:56:52 +0200

    adds the day and date to daily view as well as the currenttimeline only on the current day

--- app/src/main/java/com/example/ba_calander/DailyCalendarView.kt
+++ app/src/main/java/com/example/ba_calander/DailyCalendarView.kt
@@ -117,21 +117,6 @@ fun DailyCalendarView(
                 }
             }
 
-            val currentDate = LocalDate.now()
-            val dayOfWeek = currentDate.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
-            val date = currentDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "$dayOfWeek, $date",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-
             // Calculate the minimum and maximum times
             var earliestStartTime: LocalTime = LocalTime.MAX
             var latestEndTime: LocalTime = LocalTime.MIN
@@ -157,7 +142,8 @@ fun DailyCalendarView(
             }
 
             // Round up the latestEndTime to the next hour
-            latestEndTime = if (latestEndTime.minute > 0) latestEndTime.plusHours(1).withMinute(0) else latestEndTime
+            latestEndTime = if (latestEndTime.minute > 0) latestEndTime.plusHours(1)
+                .withMinute(0) else latestEndTime
 
             // Create a list of times from the minimum time to the maximum time with an interval of 1 hour
             val times = mutableListOf<LocalTime>()
@@ -170,7 +156,34 @@ fun DailyCalendarView(
             times.sort()
 
             // Calculate the height of each hour block
-            val hourHeight = if (times.isNotEmpty()) screenHeight / times.size else 60.dp // Default height if times list is empty
+            val hourHeight =
+                if (times.isNotEmpty()) screenHeight / times.size else 60.dp // Default height if times list is empty
+
+            // Determine the number of days to display in the pager. This could be the number of unique days in your events list.
+            val numDays = events.map { event ->
+                Instant.ofEpochMilli(event.start.toLong() * 1000)
+                    .atZone(ZoneId.of("Europe/Berlin"))
+                    .toLocalDate()
+            }.distinct().count()
+
+            val pagerState = rememberPagerState(pageCount = { numDays })
+
+            // Calculate the date for the current page
+            val currentDate = LocalDate.now().plusDays(pagerState.currentPage.toLong())
+            val dayOfWeek =
+                currentDate.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
+            val date = currentDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "$dayOfWeek, $date",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
 
             Row(
                 modifier = Modifier.fillMaxSize(),
@@ -185,8 +198,8 @@ fun DailyCalendarView(
                     times.forEach { time ->
                         Box(
                             modifier = Modifier
-                                .height(hourHeight), // Set the height of each hour block
-                            contentAlignment = Alignment.TopStart
+                                .height(hourHeight) // Set the height of each hour block
+                                .offset(y = -(hourHeight / 5)) // Move the time label up by half of the hourHeight
                         ) {
                             Text(
                                 text = formatter.format(time),
@@ -195,15 +208,6 @@ fun DailyCalendarView(
                         }
                     }
                 }
-
-                // Determine the number of days to display in the pager. This could be the number of unique days in your events list.
-                val numDays = events.map { event ->
-                    Instant.ofEpochMilli(event.start.toLong() * 1000)
-                        .atZone(ZoneId.of("Europe/Berlin"))
-                        .toLocalDate()
-                }.distinct().count()
-
-                val pagerState = rememberPagerState(pageCount = { numDays })
 
                 HorizontalPager(state = pagerState) { page ->
                     // Calculate the date for the current page
@@ -227,46 +231,69 @@ fun DailyCalendarView(
                             EventItem(event, hourHeight, earliestStartTime)
                         }
 
-                                            // Calculate the total minutes between the earliest start time and the latest end time
-                    val totalMinutes = if (earliestStartTime != null && latestEndTime != null) {
-                        val startCalendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin")).apply {
-                            time = SimpleDateFormat("HH:mm").parse(earliestStartTime.toString())
-                        }
-                        val endCalendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin")).apply {
-                            time = SimpleDateFormat("HH:mm").parse(latestEndTime.toString())
-                        }
-                        val hours = endCalendar.get(Calendar.HOUR_OF_DAY) - startCalendar.get(Calendar.HOUR_OF_DAY)
-                        hours * 60
-                    } else {
-                        24 * 60 // Total minutes in a day
-                    }
+                        // Only show the current timeline on the current day
+                        if (date == LocalDate.now()) {
+                            // Calculate the total minutes between the earliest start time and the latest end time
+                            val totalMinutes =
+                                if (earliestStartTime != null && latestEndTime != null) {
+                                    val startCalendar =
+                                        Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
+                                            .apply {
+                                                time = SimpleDateFormat("HH:mm").parse(
+                                                    earliestStartTime.toString()
+                                                )
+                                            }
+                                    val endCalendar =
+                                        Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
+                                            .apply {
+                                                time =
+                                                    SimpleDateFormat("HH:mm").parse(latestEndTime.toString())
+                                            }
+                                    val hours =
+                                        endCalendar.get(Calendar.HOUR_OF_DAY) - startCalendar.get(
+                                            Calendar.HOUR_OF_DAY
+                                        )
+                                    hours * 60
+                                } else {
+                                    24 * 60 // Total minutes in a day
+                                }
 
-                    // Calculate the current time in minutes
-                    val currentTime = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
-                    val minutes = currentTime.get(Calendar.HOUR_OF_DAY) * 60 + currentTime.get(Calendar.MINUTE)
+                            // Calculate the current time in minutes
+                            val currentTime =
+                                Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
+                            val minutes =
+                                currentTime.get(Calendar.HOUR_OF_DAY) * 60 + currentTime.get(
+                                    Calendar.MINUTE
+                                )
 
-                    // Calculate the position of the current time line
-                    val position = if (earliestStartTime != null && latestEndTime != null) {
-                        val startMinutes = SimpleDateFormat("HH:mm").parse(earliestStartTime.toString()).let {
-                            val calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
-                            calendar.time = it
-                            calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE)
+                            // Calculate the position of the current time line
+                            val position = if (earliestStartTime != null && latestEndTime != null) {
+                                val startMinutes =
+                                    SimpleDateFormat("HH:mm").parse(earliestStartTime.toString())
+                                        .let {
+                                            val calendar =
+                                                Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"))
+                                            calendar.time = it
+                                            calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(
+                                                Calendar.MINUTE
+                                            )
+                                        }
+                                // Use the state variable in the calculation to ensure that it is recalculated every minute
+                                (minutes - startMinutes + triggerRecomposition).toDouble() / totalMinutes
+                            } else {
+                                // Use the state variable in the calculation to ensure that it is recalculated every minute
+                                (minutes + triggerRecomposition).toDouble() / (24 * 60)
+                            }
+
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(2.dp)
+                                    .align(Alignment.TopStart)
+                                    .offset(y = (position * screenHeight.value).dp)
+                                    .background(Color.Red)
+                            )
                         }
-                        // Use the state variable in the calculation to ensure that it is recalculated every minute
-                        (minutes - startMinutes + triggerRecomposition).toDouble() / totalMinutes
-                    } else {
-                        // Use the state variable in the calculation to ensure that it is recalculated every minute
-                        (minutes + triggerRecomposition).toDouble() / (24 * 60)
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(2.dp)
-                            .align(Alignment.TopStart)
-                            .offset(y = (position * screenHeight.value).dp)
-                            .background(Color.Red)
-                    )
                     }
                 }
             }
